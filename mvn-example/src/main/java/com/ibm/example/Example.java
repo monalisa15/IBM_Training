5999021cdbe3034457fd7c712186bfc57db5c44c
ibmjavafsdmr10 <ibmjavafsdmr10@Javafsdnh277.iiht.tech>
2021-04-06 16:27:35 +0530

    Maven Example

---
+++ mvn-example/src/main/java/com/ibm/example/Example.java
@@ -0,0 +1,37 @@
+package com.ibm.example;
+
+public class Example {
+	
+
+	public int add(int a, int b) {
+
+		return a+b;
+
+	}
+
+	
+
+	public int subs(int a, int b) {
+
+		return a-b;
+
+	}
+
+	
+
+	public int div(int a, int b) {
+
+		return a/b;
+
+	}
+
+	
+
+	public int square(int a) {
+
+		return a*a;
+
+	}
+
+
+}
