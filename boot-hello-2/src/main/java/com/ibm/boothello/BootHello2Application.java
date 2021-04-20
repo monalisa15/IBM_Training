f4e0741602d834a824b34eb4443d582bd5f6483d
ibmjavafsdmr10 <ibmjavafsdmr10@Javafsdnh277.iiht.tech>
2021-04-20 16:24:04 +0530

    Commited twice

---
+++ boot-hello-2/src/main/java/com/ibm/boothello/BootHello2Application.java
@@ -0,0 +1,13 @@
+package com.ibm.boothello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootHello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootHello2Application.class, args);
+	}
+
+}
---
+++ boot-hello-2/src/main/java/com/ibm/boothello/bean/Login.java
@@ -0,0 +1,34 @@
+package com.ibm.boothello.bean;
+public class Login {
+	
+	private String userid;
+	private String passwd;
+	
+	public Login() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Login(String userid, String passwd) {
+		this.userid = userid;
+		this.passwd = passwd;
+	}
+
+
+	public String getUserid() {
+		return userid;
+	}
+	
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	
+	public String getPasswd() {
+		return passwd;
+	}
+	
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+}
